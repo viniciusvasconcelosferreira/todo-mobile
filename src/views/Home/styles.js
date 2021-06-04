f66022c31fb00227798cc51d74b7465abaac533a
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-06-04 12:26:42 -0300

    Tela home e estrutura de pastas e arquivos

---
+++ src/views/Home/styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, //tela inteira
+        backgroundColor: '#FFF',
+        //alinhamento na horizontal
+        alignItems: 'center',
+        //alinhamento na vertical
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
