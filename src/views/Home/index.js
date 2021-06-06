2fef4d0d87d1eb7811fa79ed7da5c5348c1a1273
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-06-06 20:48:09 -0300

    Footer dinâmico

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,11 +3,13 @@ import {Text, View} from 'react-native';
 import styles from "./styles";
 //COMPONENTES
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function Home() {
     return (
         <View style={styles.container}>
             <Header showNotification={true}/>
+            <Footer icon={'add'}/>
         </View>
     )
 }
