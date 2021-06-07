f95a2d359f2c282f2d69149722ce160028b0fad9
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-06-06 21:28:46 -0300

    Filtros

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -2,12 +2,32 @@ import {StyleSheet} from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, //tela inteira
+        //tela inteira
+        flex: 1,
         backgroundColor: '#FFF',
         //alinhamento na horizontal
         alignItems: 'center',
         //alinhamento na vertical
         justifyContent: 'flex-start'
+    },
+    filter: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        height: 70,
+        alignItems: 'center'
+    },
+    filterTextActived: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#EE6B26'
+    },
+    filterTextInative: {
+        color: '#20295F',
+        fontWeight: 'bold',
+        fontSize: 18,
+        //intervalo de 0 a 1
+        opacity: 0.5
     }
 });
 
