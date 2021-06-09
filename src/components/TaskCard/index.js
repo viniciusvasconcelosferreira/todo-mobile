8b384e501bcf08eb3528efb8a9e455fd928669c1
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-06-08 23:46:34 -0300

    Icones dinâmicos na listagem das tarefas

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import {Text, View, Image, TouchableOpacity} from "react-native";
 import styles from './styles';
-import iconDefault from '../../assets/default.png'
+//COLEÇÃO DE ICONES
+import typeIcons from '../../utils/typeIcons'
+import iconDefault from '../../assets/default.png';
 import {format} from 'date-fns';
 
-export default function TaskCard({done, title, when}) {
+export default function TaskCard({done, title, when, type}) {
     return (
         <TouchableOpacity style={[styles.card, done && styles.done]}>
             <View style={styles.cardLeft}>
-                <Image source={iconDefault} style={styles.typeActive}/>
+                <Image source={typeIcons[type]} style={styles.typeActive}/>
                 <Text style={styles.cardTitle}>{title}</Text>
             </View>
             <View style={styles.cardRight}>
-                <Text style={styles.cardDate}>{format(new Date(when),'dd/MM/yyyy')}</Text>
-                <Text style={styles.cardTime}>{format(new Date(when),'HH:mm')}</Text>
+                <Text style={styles.cardDate}>{format(new Date(when), 'dd/MM/yyyy')}</Text>
+                <Text style={styles.cardTime}>{format(new Date(when), 'HH:mm')}</Text>
             </View>
         </TouchableOpacity>
     )
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -51,8 +51,8 @@ export default function Home() {
                 {load ?
                     <ActivityIndicator color='#EE6B26' size={50}/>
                     :
-                    tasks.map((t,index) => (
-                        <TaskCard done={false} title={t.title} when={t.when}/>
+                    tasks.map(t => (
+                        <TaskCard done={false} title={t.title} when={t.when} type={t.type}/>
                     ))
                 }
 
